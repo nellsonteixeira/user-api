2316634abd014c717a7d4dc331ebd52d439c03fb
nellsonteixeira <30749385+nellsonteixeira@users.noreply.github.com>
2021-11-10 17:45:00 -0300

    Update user.go
    Remove Address struct

--- models/user.go
+++ models/user.go
@@ -4,10 +4,5 @@ type User struct {
 	Id        int      `json:"id,omitempty"`
 	Firstname string   `json:"firstname,omitempty"`
 	Lastname  string   `json:"lastname,omitempty"`
-	Address   *Address `json:"address,omitempty"`
 }
 
-type Address struct {
-	City  string `json:"city,omitempty"`
-	State string `json:"state,omitempty"`
-}
